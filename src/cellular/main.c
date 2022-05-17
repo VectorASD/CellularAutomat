58592798c3245e490df27a27173da97e3a19a7e1
Prilepa Mixhail <prilepa.mixh@mail.ru>
2022-05-18 00:25:06 +0700

    The first triangle on the shaders. VBO + VAO technology is used

--- src/cellular/main.c
+++ src/cellular/main.c
@@ -5,52 +5,145 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef unsigned char byte;
+typedef const char *text;
+
 //apt-get install libglew-dev libglfw3-dev libglm-dev
 //Главное в будущем не забыть перенести куда-нибудь в Readme это перед отправкой на проверку
 //LFLAGS = ... -lm -lX11 -lpthread -lXrandr -ldl Возможно в будущем что-то из этого понадобиться...
 
-int main(int argc, char *argv[]) {
-    printf("Hello, Meaterson!\n");
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode) {
+    printf("key: %3d   scancode: %2d   action: %d   mode: %2d\n", key, scancode, action, mode);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
+text vertex_shader_source = R"glsl(
+    #version 330 core
+    layout (location = 0) in vec3 position;
+    void main() {
+        gl_Position = vec4(position.x, position.y, position.z, 1);
+    }
+)glsl";
+
+text fragment_shader_source = R"glsl(
+    #version 330 core
+    out vec4 color;
+    void main() {
+	color = vec4(1, 0.5, 0.2, 1);
+    }
+)glsl";
+
+byte compile_shader(GLuint shader) {
+    glCompileShader(shader);
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        GLchar info_log[512];
+        glGetShaderInfoLog(shader, 512, NULL, info_log);
+        printf("vertexShader ошибка компиляции: %s\n", info_log);
+        return 1;
+    }
+    return 0;
+}
+
+byte check_program(GLuint program) {
+    glLinkProgram(program);
+    GLint success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        GLchar info_log[512];
+        glGetProgramInfoLog(program, 512, NULL, info_log);
+        printf("Ошибка слияния шейдеров: %s\n", info_log);
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
     if (!glfwInit()) {
-        printf("Failed to initialize GLFW\n");
+        printf("Инициализация GLFW провалена\n");
         getchar();
-        return -1;
+        return 1;
     }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Cellular Automat by VectorASD & ", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Cellular Automat by VectorASD & Mapyax", NULL, NULL);
     if (window == NULL) {
-        printf("Failed to create GLFW window\n");
+        printf("Создание GLFW окна провалено\n");
         getchar();
         glfwTerminate();
-        return -1;
+        return 2;
     }
     glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
 
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
-        printf("Failed to initialize GLEW\n");
+        printf("Инициализация GLEW провалена\n");
         getchar();
         glfwTerminate();
-        return -1;
+        return 3;
     }
 
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    if (compile_shader(vertex_shader)) return 4;
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    if (compile_shader(fragment_shader)) return 5;
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    if (check_program(shader_program)) return 6;
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    GLfloat vertices[] = {
+        -0.5, -0.5, 0,
+        0.5, -0.5, 0,
+        0, 0.5, 0};
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *) 0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    glClearColor(0, 0.5, 1, 0);
     do {
+        glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
         glfwSwapBuffers(window);
-        glfwPollEvents();
 
     } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
-    
+
     glfwTerminate();
-    
+
     return 0;
 }
